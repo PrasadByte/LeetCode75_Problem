afece055fc9cb9d8fbc55c97f19b78b79e898d31
prasad <prasadpatila99@gmail.com>
2025-02-16 23:24:30 +0530

    basic

---
+++ LeetCode_75/src/basic/Factorial.java
@@ -0,0 +1,17 @@
+	package basic;
+	
+	public class Factorial {
+		
+		public static int factorialNo(int num) {
+			int temp=1;
+			for(int i=1;i<=num;i++) {
+				temp *= i;
+			}
+			return temp;
+		}
+		
+		public static void main(String[] args) {
+			System.out.println(factorialNo(5));
+		}
+	
+	}
---
+++ LeetCode_75/src/basic/fibonaciSeries.java
@@ -0,0 +1,29 @@
+
+	
+
+package basic;
+
+import java.util.Scanner;
+
+public class fibonaciSeries {
+
+    public static void fibonaciee(int num) {
+        int num1 = 1;
+        int num2 = 2;
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            System.out.println(num1);
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        int num = sc.nextInt();
+        fibonaciee(num);
+        sc.close();
+    }
+}
