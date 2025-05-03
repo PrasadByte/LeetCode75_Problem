2330fbf229c5a27b35d8373b493afd6e539b9330
prasad <prasadpatila99@gmail.com>
2025-05-03 23:57:53 +0530

    valid paranthes changes

--- LeetCode_75/src/leetcode75/LongestValidParentheses.java
+++ LeetCode_75/src/leetcode75/LongestValidParentheses.java
@@ -3,40 +3,50 @@ package leetcode75;
 import java.util.Stack;
 
 public class LongestValidParentheses {
+
+    // Function to compute the longest valid parentheses substring
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
-        stack.push(-1); // Initialize base for length calculation
-        int max_len = 0;
+        stack.push(-1); // Base value to help calculate valid lengths
+        int max_len = 0; // To track the maximum length of valid parentheses
 
+        // Traverse the string character by character
         for (int i = 0; i < s.length(); i++) {
+            // If we find an opening bracket, push its index to stack
             if (s.charAt(i) == '(') {
                 stack.push(i);
             } else {
+                // Pop the last open bracket index
                 stack.pop();
+
+                // If stack becomes empty, push current index as base
                 if (stack.isEmpty()) {
                     stack.push(i);
                 } else {
+                    // Calculate the length of the current valid substring
                     max_len = Math.max(max_len, i - stack.peek());
                 }
             }
         }
 
-        return max_len;
+        return max_len; // Return the maximum length found
     }
 
-    // Main method to test the function
+    // Main method to run test cases
     public static void main(String[] args) {
         LongestValidParentheses solution = new LongestValidParentheses();
 
-        // Example test cases
+        // Sample test cases
         String s1 = "(()";
         String s2 = ")()())";
         String s3 = "(()())";
         String s4 = "";
+        String s5 = "((())())())";
 
         System.out.println("Input: \"" + s1 + "\" => Longest Valid Parentheses: " + solution.longestValidParentheses(s1));
         System.out.println("Input: \"" + s2 + "\" => Longest Valid Parentheses: " + solution.longestValidParentheses(s2));
         System.out.println("Input: \"" + s3 + "\" => Longest Valid Parentheses: " + solution.longestValidParentheses(s3));
         System.out.println("Input: \"" + s4 + "\" => Longest Valid Parentheses: " + solution.longestValidParentheses(s4));
+        System.out.println("Input: \"" + s5 + "\" => Longest Valid Parentheses: " + solution.longestValidParentheses(s5));
     }
 }
--- LeetCode_75/src/leetcode75/Practice.java
+++ LeetCode_75/src/leetcode75/Practice.java
@@ -1,26 +1,18 @@
 package leetcode75;
 
+import java.util.Stack;
+
 public class Practice {
-	public static int[] productExceptItselft(int[] num) {
-		int n = num.length;
-		int[] ans = new int[n];
-		int left[] = new int[n];
-		left[0] = 1;
-		for (int i = 1; i < n; i++) {
-			left[i] = left[i - 1] * num[i - 1];
-		}
-		int right = 1;
-		for (int i = n - 1; i >= 0; i--) {
-			ans[i] = left[i] * right;
-			right *= num[i];
-		}
-		return ans;
-	}
-	public static void main(String[] args) {
-		int[] num = { 1, 2, 3, 4 };
-		int[] ans = productExceptItselft(num);
-		for (int i = 0; i < ans.length; i++) {
-			System.out.print(ans[i] + " ");
+public int LongestValid(String s){
+	int max =0;
+	Stack<Integer>stack=new Stack<>();
+	stack.push(-1);
+	for int (int i=0;i<s.length();i++){
+		if(s.charAt(i)=='('){
+			stack.push(i);
+			else{}
 		}
 	}
+	retrun 0;
+}
 }
