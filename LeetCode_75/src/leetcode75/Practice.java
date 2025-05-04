d740b1c4cc8ae4a4358f76893e6fe8912007fe94
prasad <prasadpatila99@gmail.com>
2025-05-04 23:27:26 +0530

    two sum

--- LeetCode_75/src/leetcode75/Practice.java
+++ LeetCode_75/src/leetcode75/Practice.java
@@ -1,18 +1,8 @@
 package leetcode75;
 
-import java.util.Stack;
 
 public class Practice {
-public int LongestValid(String s){
-	int max =0;
-	Stack<Integer>stack=new Stack<>();
-	stack.push(-1);
-	for int (int i=0;i<s.length();i++){
-		if(s.charAt(i)=='('){
-			stack.push(i);
-			else{}
-		}
-	}
-	retrun 0;
+
+	
 }
-}
+
---
+++ LeetCode_75/src/leetcode75/TwoSum.java
@@ -0,0 +1,37 @@
+
+package leetcode75;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // If complement is found in the map, return indices
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            // Store the current number and its index in the map
+            map.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
+}
