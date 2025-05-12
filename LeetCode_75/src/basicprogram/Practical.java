b848d53f2b79c0a05b727a3296608a6947a81fd7
prasad <prasadpatila99@gmail.com>
2025-05-12 23:09:56 +0530

    basic code added:

--- LeetCode_75/src/basicprogram/Practical.java
+++ LeetCode_75/src/basicprogram/Practical.java
@@ -1,21 +1,19 @@
 package basicprogram;
 
-import java.util.Scanner;
-
 public class Practical {
-	public static boolean isArraySorted(int []nums) {
-		for (int i = 1; i < nums.length; i++) {
-			if(nums[i]>=nums[i-1]) {
-			System.out.println("condition");
-			}
-			else {
-				return false;
-			}
-		}
-		return true;
-	}
-	public static void main(String[] args) {
-		int []arr= {1,2,3,65,4,5,6,7,8,9};
-		System.out.println(isArraySorted(arr));
-	}
+    public static int digitcount(int num) {
+        int count = 0;
+        
+        // Loop runs as long as num is greater than 0
+        for (; num > 0; num /= 10) {
+            count++;  // Increment count for each digit
+        }
+        
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int digit = 657732;
+        System.out.println("Number of digits: " + digitcount(digit));
+    }
 }
