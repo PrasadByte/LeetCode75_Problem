11dd5f6427b33ce139ccd2dae76b3957be0d3648
prasad <prasadpatila99@gmail.com>
2025-05-15 21:50:20 +0530

    practice

--- LeetCode_75/src/basicprogram/Practical.java
+++ LeetCode_75/src/basicprogram/Practical.java
@@ -1,17 +1,7 @@
 package basicprogram;
 
 public class Practical {
-	public static int countDigit(int num) {
-		int count = 0;
-		while(num> 0) {
-			num /=10;
-			count++;
-			
-		}
-		return count;
-		
-	}
-	public static void main(String[] args) {
-		
-	}
+	
+	
+
 }
