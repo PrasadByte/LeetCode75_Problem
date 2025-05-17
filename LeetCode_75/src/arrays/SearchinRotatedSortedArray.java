19d4ea3dd6a89e6f7731607da0a922c9753048d2
prasad <prasadpatila99@gmail.com>
2025-05-17 23:38:46 +0530

    Binary Search id added

---
+++ LeetCode_75/src/arrays/SearchinRotatedSortedArray.java
@@ -0,0 +1,41 @@
+package arrays;
+
+public class SearchinRotatedSortedArray {
+	 public static int search(int[] nums, int target) {
+         int left = 0;
+      int right = nums.length - 1;
+
+      while (left <= right) {
+          int mid = (left + right) / 2;
+
+          if (nums[mid] == target) {
+              return mid;
+          } else if (nums[mid] >= nums[left]) {
+              if (nums[left] <= target && target <= nums[mid]) {
+                  right = mid - 1;
+              } else {
+                  left = mid + 1;
+              }
+          } else {
+              if (nums[mid] <= target && target <= nums[right]) {
+                  left = mid + 1;
+              } else {
+                  right = mid - 1;
+              }
+          }
+      }
+
+      return -1; 
+  }
+	 public static void main(String[] args) {
+		int nums[] = {4,5,6,7,0,1,2};
+		int target = 0;
+		int result = search(nums, target);
+		if(result == -1) {
+			System.out.println("Element not found");
+	}
+		else {
+			System.out.println("Element found at index: " + result);
+		}
+	}
+}
