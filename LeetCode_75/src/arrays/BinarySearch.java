4b499d4364c2d660374c59c1c718741612d95e44
prasad <prasadpatila99@gmail.com>
2025-05-19 23:38:53 +0530

    Binary Search practice

---
+++ LeetCode_75/src/arrays/BinarySearch.java
@@ -0,0 +1,43 @@
+package arrays;
+
+public class BinarySearch {
+	 int binarySearch(int arr[], int x)
+	    {
+	        int left = 0, right = arr.length - 1;
+	        while (left <= right) {
+	            int mid =  (left + right) / 2;
+
+	            // Check if x is present at mid
+	            if (arr[mid] == x)
+	                return mid;
+
+	            // If x greater, ignore left half
+	            if (arr[mid] < x)
+	                left = mid + 1;
+
+	            // If x is smaller, ignore right half
+	            else
+	                right = mid - 1;
+	        }
+
+	        // If we reach here, then element was
+	        // not present
+	        return -1;
+	    }
+
+	    // Driver code
+	    public static void main(String args[])
+	    {
+	        BinarySearch ob = new BinarySearch();
+	        int arr[] = { 2, 3, 4, 10, 40 };
+	        int n = arr.length;
+	        int x = 10;
+	        int result = ob.binarySearch(arr, x);
+	        if (result == -1)
+	            System.out.println(
+	                "Element is not present in array");
+	        else
+	            System.out.println("Element is present at "
+	                               + "index " + result);
+	    }
+	}
--- LeetCode_75/src/arrays/Practice.java
+++ LeetCode_75/src/arrays/Practice.java
@@ -3,6 +3,6 @@ package arrays;
 public class Practice {
 	public static int search(int [] num,int target) {
 		int left =0;
-		
+		int right = num.length-1;
 	}
 }
