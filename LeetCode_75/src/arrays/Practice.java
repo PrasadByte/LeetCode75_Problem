56ac3d4abdfa810c8665a6c22155ca6691a369b5
prasad <prasadpatila99@gmail.com>
2025-05-21 20:55:04 +0530

    practice

--- LeetCode_75/src/arrays/Practice.java
+++ LeetCode_75/src/arrays/Practice.java
@@ -1,24 +1,33 @@
 package arrays;
 
-public class Practice {
-    public static int binarySearch(int []arr, int target) {
-    	int left =0;
-    	int right = arr.length-1;
-    	while(left<=right) {
-    		int mid = left + ( right - left)/2;
-    		if(arr[mid]== target) {
-    			return mid;
-    		}else if( arr[mid]> target) {
-    			  right = mid -1;
-    		}else
-    			left = mid +1;
+import java.util.Scanner;
 
-    	}
-    	return -1;
-    }
-    public static void main(String[] args) {
-		int [] num= {1,2,3,4,5,67,90};
-		int target =67;
-		System.out.println("Element found at "+binarySearch(num, target));	
+public abstract class Practice {
+	public static int binarySerach(int[] arr, int target) {
+		int left = 0;
+		int right = arr.length - 1;
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (arr[mid] == target) {
+				return mid;
+			} else if (arr[mid] > target) {
+				right = mid - 1;
+
+			} else {
+				left = mid + 1;
+			}
+		}
+		return -1;
 	}
+
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 System.out.println("Enter the size of the array");
+	 int size = sc.nextInt();
+	 int [] arr= new int [size];
+	
+	 System.out.println("Enter the element to find");
+	 int target= sc.nextInt();
+	System.out.println(binarySerach(arr, target));	 
+  }
 }
