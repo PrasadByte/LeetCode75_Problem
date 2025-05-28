7f3a7da81c436c0aa7d25c0610395e34f81ca75d
prasad <prasadpatila99@gmail.com>
2025-05-28 22:42:17 +0530

    two sum

--- LeetCode_75/src/arrays/ArrayInput.java
+++ LeetCode_75/src/arrays/ArrayInput.java
@@ -1,6 +1,13 @@
 package arrays;
 
+//import java.util.Scanner;
+import java.util.Scanner;
+
+
 public class ArrayInput {
-	//bn
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	
+	}
 
 }
