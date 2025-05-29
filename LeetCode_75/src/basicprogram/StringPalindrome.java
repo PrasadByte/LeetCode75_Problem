4df85d07777953b557f65b558318b52b34e06748
prasad <prasadpatila99@gmail.com>
2025-05-29 23:49:09 +0530

    basic

--- LeetCode_75/src/basicprogram/StringPalindrome.java
+++ LeetCode_75/src/basicprogram/StringPalindrome.java
@@ -4,23 +4,27 @@ public class StringPalindrome {
 	public static void reverString(String str) {
 		int left =0;
 		int right = str.length()-1;
-		while(left<right) {
+		for (int i = 0; i < str.length(); i++) {
 			if(str.charAt(right)!= str.charAt(left)) {
 				System.out.println(" string is not palindrome");
+			}
+			else {
+				System.out.println("String is palindrome");
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		String str = "madam";
-		String reverse = "";
-		for (int i = str.length() - 1; i >= 0; i--) {
-			reverse = reverse + str.charAt(i);
-		}
-		if (str.equals(reverse)) {
-			System.out.println("String is palindrome");
-		} else {
-			System.out.println("String is not palindrome");
-		}
+		String str = "madaarem";
+//		String reverse = "";
+//		for (int i = str.length() - 1; i >= 0; i--) {
+//			reverse = reverse + str.charAt(i);
+//		}
+//		if (str.equals(reverse)) {
+//			System.out.println("String is palindrome");
+//		} else {
+//			System.out.println("String is not palindrome");
+//		}
+		reverString(str);
 	}
 }
