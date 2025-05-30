37dbefc5fc8bfc4396a5f1ed4467a31e31b66210
prasad <prasadpatila99@gmail.com>
2025-05-30 23:48:49 +0530

    basic

--- LeetCode_75/src/arrays/Practice.java
+++ LeetCode_75/src/arrays/Practice.java
@@ -3,27 +3,26 @@ package arrays;
 import java.util.HashMap;
 
 public class Practice {
-	public static int [] twoSum(int []num,int target) {
-		HashMap<Integer, Integer>map=new HashMap<>();
-		for (int i = 0; i < num.length; i++) {
-		int	complement = target - num[i];
-		if(map.containsKey(complement)) {
-			return new int[] {map.get(complement), i};
+	public static int binarySearch(int [] nums, int target) {
+		int left =0;
+		int right=nums.length-1;
+		while(left<=right) {
+			int mid = left +(right- left) /2;
+			if(nums[mid] == target ) {
+				return mid;
+			}
+			if(nums[mid]>right) {
+				right = mid -1;
+			}else {
+				left= mid  +1;
+			}
+				
 		}
-		map.put(num[i], i);
-		}
-		return new int [] {};
+		return -1;
 	}
 	public static void main(String[] args) {
-		int [] arr = {1,3,4,6,8,9};
-		int target =17;
-		int [] result =twoSum(arr, target);
-		if(result.length==2) {
-			 System.out.println("Indices: [" + result[0] + ", " + result[1 ] + "]");
-			System.out.println(result[0] +" "+ result[1]);
-		}else {
-			System.out.println("no sum solution found");
-		}
-		System.out.println(twoSum(arr, target));
+		int [] arr= {1,2,3,4,5,6,7};
+		int target =3;
+		binarySearch(arr, target);
 	}
 }
