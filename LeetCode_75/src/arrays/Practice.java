009fe66eb5bb758e207d260d8e8680882aa1aa3a
prasad <prasadpatila99@gmail.com>
2025-06-02 22:39:20 +0530

    basic

--- LeetCode_75/src/arrays/Practice.java
+++ LeetCode_75/src/arrays/Practice.java
@@ -1,28 +1,8 @@
 package arrays;
 
-import java.util.HashMap;
 
 public class Practice {
-	public static int binarySearch(int [] nums, int target) {
-		int left =0;
-		int right=nums.length-1;
-		while(left<=right) {
-			int mid = left +(right- left) /2;
-			if(nums[mid] == target ) {
-				return mid;
-			}
-			if(nums[mid]>right) {
-				right = mid -1;
-			}else {
-				left= mid  +1;
-			}
-				
-		}
-		return -1;
-	}
-	public static void main(String[] args) {
-		int [] arr= {1,2,3,4,5,6,7};
-		int target =3;
-		binarySearch(arr, target);
-	}
+
 }
+
+	
